c8cd646c25888fd77c802672203982e346113355
Nurullah1235 <67196465+Nurullah1235@users.noreply.github.com>
2020-07-05 21:49:50 +0300

    Update ClassOne.java
    ilk commit remote feature branchdan

--- src/git_proje04/ClassOne.java
+++ src/git_proje04/ClassOne.java
@@ -5,6 +5,9 @@ public class ClassOne {
 	public static void main(String[] args) {
 		
 		System.out.println("ilk commit local mastredan");
+		System.out.println("ilk commit local mastredan")
+		
+		
 		
 
 	
